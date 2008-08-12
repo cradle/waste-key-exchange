e78c90b1c799b52a68c3cf005819b3c307d0a80c
Glenn Murray <glenn@trike.com.au>
2008-08-12 23:36:26 +1000

    Updated parser, used next instead of readline

--- parser.py
+++ parser.py
@@ -2,19 +2,20 @@ class Key(object):
   token = 'WASTE_PUBLIC_KEY'
 
   def __init__(self, io):
-    line = io.readline().strip()
+    line = io.next().strip()
     while not line.startswith(Key.token):
-      line = io.readline().strip()
+      line = io.next().strip()
     elements = line.split(None, 3)
     self.number = long(elements[1])
     self.length = long(elements[2])
     self.name = elements[3]
     public = []
-    line = io.readline().strip()
+    line = io.next().strip()
     while not line.startswith(Key.token):
       public.append(line)
-      line = io.readline().strip()
+      line = io.next().strip()
     self.public = public
+      
 
   def __str__(self):
     return """%s %s %s %s
